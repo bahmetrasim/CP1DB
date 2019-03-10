36b791e340d8355812cb0dfdf25f07245753d272
Ahmet Rasim Bozkurt <b.ahmetrasim@gmail.com>
2019-03-10 22:34:39 +0300

    AdMob Youtube - Google Service

---
+++ CP1/CP1.Android/AdBanner_Droid.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using AdMob;
+using Android.Gms.Ads;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AdMob.CustomRenders;
+using AdMob.Droid.CustomRenders;
+using Android.Content;
+
+[assembly: ExportRenderer(typeof(AdBanner), typeof(AdBanner_Droid))]
+namespace AdMob.Droid.CustomRenders
+{
+    public class AdBanner_Droid : ViewRenderer
+    {
+        Context context;
+        public AdBanner_Droid(Context _context) : base(_context)
+        {
+            context = _context;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                var adView = new AdView(Context);
+                switch ((Element as AdBanner).Size)
+                {
+                    case AdBanner.Sizes.Standardbanner:
+                        adView.AdSize = AdSize.Banner;
+                        break;
+                    case AdBanner.Sizes.LargeBanner:
+                        adView.AdSize = AdSize.LargeBanner;
+                        break;
+                    case AdBanner.Sizes.MediumRectangle:
+                        adView.AdSize = AdSize.MediumRectangle;
+                        break;
+                    case AdBanner.Sizes.FullBanner:
+                        adView.AdSize = AdSize.FullBanner;
+                        break;
+                    case AdBanner.Sizes.Leaderboard:
+                        adView.AdSize = AdSize.Leaderboard;
+                        break;
+                    case AdBanner.Sizes.SmartBannerPortrait:
+                        adView.AdSize = AdSize.SmartBanner;
+                        break;
+                    default:
+                        adView.AdSize = AdSize.Banner;
+                        break;
+                }
+                // TODO: change this id to your admob id  
+                adView.AdUnitId = "ca-app-pub-9496442035641158/7670378711";
+                var requestbuilder = new AdRequest.Builder();
+                adView.LoadAd(requestbuilder.Build());
+                SetNativeControl(adView);
+            }
+        }
+    }
+}
